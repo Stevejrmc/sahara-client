3aa5348876cd531914268a3911e4afceec03731d
Steve McLaughlin <stevejrmc@gmail.com>
2020-02-16 22:58:51 -0800

    Add and configure less

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       use: ['babel-loader', 'eslint-loader']
+    },
+    {
+      test: /\.less$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'less-loader'
+      ]
     }
     ]
   },
